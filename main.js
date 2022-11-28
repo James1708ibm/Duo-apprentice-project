23b3fcbf5faa2b943bd55a82457f03a20f76b685
James1708ibm <105230841+James1708ibm@users.noreply.github.com>
2022-11-28 04:09:11 +0000

    11/21/22 Version

---
+++ main.js
@@ -0,0 +1,5 @@
+const Mute_Button = document.querySelector('#Mute_Button');
+Mute_Button.addEventListener('click', Sound_Control);
+function Sound_Control(){
+    let toggleMute = document.querySelector('.Sound_Control');
+    toggleMute.muted = !toggleMute.muted;
