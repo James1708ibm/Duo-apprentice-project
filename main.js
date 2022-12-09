d7974d76ee6a66d70b4b8d0c5d68b81ddb3e01fb
Taylor Wilson <Taylor.Wilson@ibm.com>
2022-12-09 14:19:26 -0600

    Changed the naming conventions for all variables

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
-const Mute_Button = document.querySelector('.Mute_Button');
-Mute_Button.addEventListener('click', Sound_Control);
-function Sound_Control() {
-    let Toggle_Mute = document.querySelector('.Sound_Control');
-    Toggle_Mute.muted = !Toggle_Mute.muted;
-
-    //console.log(Mute_Button.src);
-
-    // if (Mute_Button.src.indexOf("Music_Note.jpg") >= 0) 
-    // {
-    //     Mute_Button.src = "Music_Note_Muted.jpg";
-    // }
-    // else {
-    //     Mute_Button.src = "Music_Note.jpg"
-    // }
-
+const MuteButton = document.querySelector('.mute-button');
+MuteButton.addEventListener('click', soundControl);
+function soundControl() {
+    let toggleMute = document.querySelector('.sound-control');
+    toggleMute.muted = !toggleMute.muted;
 }
 
 // when the mute button is clicked the source image is changed to Music note muted and reverted if clicked again
-Mute_Button.addEventListener('click', Mute_Button_Transition);
-function Mute_Button_Transition() {
-    if (Mute_Button.src.indexOf("Music_Note.jpg") >= 0) 
+MuteButton.addEventListener('click', muteButtonTransition);
+function muteButtonTransition() {
+    if (MuteButton.src.indexOf("Music_Note.jpg") >= 0) 
     {
-        Mute_Button.src = "Music_Note_Muted.jpg";
+        MuteButton.src = "Music_Note_Muted.jpg";
     }
     else {
-        Mute_Button.src = "Music_Note.jpg";
+        MuteButton.src = "Music_Note.jpg";
     }
 }
-setTimeout()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------
 /*let window;
 
 main(passedwindow){
